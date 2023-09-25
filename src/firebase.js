425dcc38df63d5519b0b50588744e19fcba6d47c
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-09-26 00:46:12 +0300

    ADD: finished header

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAJDRJvuQKCMjn1aiO-6jPWClMrc60VVM4",
+//   authDomain: "challenge-74372.firebaseapp.com",
+//   projectId: "challenge-74372",
+//   storageBucket: "challenge-74372.appspot.com",
+//   messagingSenderId: "794278267769",
+//   appId: "1:794278267769:web:abf90eb48108a89dcdfda8",
+//   measurementId: "G-CJF6Y5QT7M"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJDRJvuQKCMjn1aiO-6jPWClMrc60VVM4",
+  authDomain: "challenge-74372.firebaseapp.com",
+  projectId: "challenge-74372",
+  storageBucket: "challenge-74372.appspot.com",
+  messagingSenderId: "794278267769",
+  appId: "1:794278267769:web:abf90eb48108a89dcdfda8",
+  measurementId: "G-CJF6Y5QT7M"
+};
